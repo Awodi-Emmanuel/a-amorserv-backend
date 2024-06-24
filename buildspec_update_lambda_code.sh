4a19dc3fbc696ca0a149d91562b6d5f33bde66c1
awodi-emmanuel <emmanuelawodi25@gmail.com>
2024-06-24 15:20:13 +0100

    Modified buildspecs Test

--- buildspec_update_lambda_code.sh
+++ buildspec_update_lambda_code.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+
 echo "Lambda files got changed. Going to update lambda function code"
-aws ssm get-parameters-by-path --path /Test-LAMBDA --region us-west-2 | jq -r '.Parameters | map(.Name+"="+.Value)| join("\n") | sub("/Test-LAMBDA/"; ""; "g")  ' >.envmkdir -p /tmp/lamda/Test
-echo Copying Files from Project to Temp Folder
-rsync -a  Test/ /tmp/lamda/Test/
-mv /tmp/lamda/Test/Test.py /tmp/lamda/Test/lambda_function.py
-cp .env /tmp/lamda/Test/.env
-cd /tmp/lamda/Test/ && zip -rq ../Test.zip .
-aws s3 cp /tmp/lamda/Test.zip s3://your-bucket/lambda_functions/Test-Lambda/Test.zip
-aws lambda update-function-code --function-name TestLambda --s3-bucket your-bucket --s3-key lambda_functions/Test-Lambda/Test.zip
+aws ssm get-parameters-by-path --path /Test-LAMBDA --region us-west-2 | jq -r '.Parameters | map(.Name+"="+.Value)| join("\n") | sub("/Test-LAMBDA/"; ""; "g")  ' > .env
+
+mkdir -p /tmp/lambda/Test
+echo "Copying Files from Project to Temp Folder"
+rsync -a lambda_code/ /tmp/lambda/Test/
+cp .env /tmp/lambda/Test/.env
+
+cd /tmp/lambda/Test/ && zip -rq ../Test.zip .
+aws s3 cp /tmp/lambda/Test.zip s3://b-amorserv-s3-codepipeline/lambda_functions/Test-Lambda/Test.zip
+aws lambda update-function-code --function-name TestLambda --s3-bucket b-amorserv-s3-codepipeline --s3-key lambda_functions/Test-Lambda/Test.zip
